efb47985d4059c9f1a6c30c804903e86efbe8679
Carlos Ferreira <carlosprovisorio@icloud.com>
2018-04-04 16:04:46 -0400

    change AlbumList functional component to class component

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
-const App = () => <Header headerText={'Autzu'} />;
+const App = () => (
+  <View>
+    <Header headerText={'Autzu'} />
+    <AlbumList />
+  </View>
+);
 
 // Render it to device
 AppRegistry.registerComponent('albums', () => App);
