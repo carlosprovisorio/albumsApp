bd6f2a83c92258f43f68c97ea074b22702e2fa82
Carlos Ferreira <carlosprovisorio@icloud.com>
2018-04-05 10:02:23 -0400

    create AlbumDetail component to render all details to the album

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry, View, YellowBox } from 'react-native';
 import Header from './src/components/Header';
 import AlbumList from './src/components/AlbumList';
 
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated'
+]);
 // Create a component
 const App = () => (
   <View>
