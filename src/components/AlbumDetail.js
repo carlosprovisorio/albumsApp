c22fd5fca1939a28f7f7e354ae375bb97c3bec47
Carlos Ferreira <carlosprovisorio@icloud.com>
2018-04-06 12:50:58 -0400

    add opePress event to handle button action

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -31,7 +31,7 @@ const AlbumDetail = ({ album }) => {
       </CardSection>
 
       <CardSection>
-        <Button />
+        <Button onPress={() => console.log(title)} />
       </CardSection>
     </Card>
   );
